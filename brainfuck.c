d0f26a2b5b14e8a45567c065d5076b71d92ef156
greg androulakis <csd3368@csd.uoc.gr>
2020-05-29 20:09:45 +0300

    paging for memory indexes used

--- brainfuck.c
+++ brainfuck.c
@@ -7,6 +7,7 @@
 #include<stdio.h>
 #include <stdlib.h>
 
+
 #define EXEC_CONTINUE 0
 #define EXEC_STOPPED_ERR 1
 #define EXEC_STOPPED_OK 2
@@ -27,8 +28,9 @@ int exec_to();
 char* seek_closing_bracket(char*);
 char* seek_opening_bracket(char*);
 
+
 int link_brackets(char*code,char**links);
-void zeros(char*arr,size_t len);
+void zerosc(char*arr,size_t len);
 void print_exec_state();
 
 /*program state:  execute / finished ok / finished with error */
@@ -38,11 +40,11 @@ char execution_state=EXEC_CONTINUE;
 char verbose=0;
 
 
-size_t block_size=4096;  /*array extension step*/
+size_t block_size=1024;  /*array extension step*/
 
 
 char *code=NULL;         /* "code segment" */
-size_t code_size=32768;
+size_t code_size=2048;
 char*cmd=NULL;           /*current instruction pointer*/
 
 
@@ -50,9 +52,114 @@ char*cmd=NULL;           /*current instruction pointer*/
 char**links=NULL;
 int lp=0;
 
-char *data=NULL;         /*"data segment*/
-size_t data_size=32768;
-char*ptr=NULL;           /*current memory pos. pointer*/
+int ptr=0;
+char dirty_ptr=0;
+struct block*blocks;
+struct block*block_cursor;
+int*block_mem=0;
+
+struct block{
+	int id;
+	int*mem;
+	struct block*next;
+	struct block*prev;
+};
+
+void printstate(){printf("cmd:%ld\tptr:%d\n",(cmd-code),(int)ptr);}
+
+struct block*new_block(int id);
+
+
+int block_of_addr(int memptr){
+	return memptr/block_size;
+}
+
+void set_cursor(struct block*b){
+	block_cursor=b;
+	block_mem=b->mem;
+	dirty_ptr=0;
+}
+
+
+
+void zeros(int*ar,unsigned int len){
+	unsigned int i=0;
+	while(i<len)
+		ar[i++]=0;
+}
+
+struct block*new_block(int id){
+	struct block*res=malloc(sizeof(struct block));
+	if(NULL==res)return NULL;
+
+	res->mem=malloc(sizeof(int)*block_size);
+	if(NULL==res->mem){
+		free(res);
+		return NULL;
+	}
+
+	zeros(res->mem,block_size);
+	res->prev = res->next = NULL;
+	res->id=id;
+	return res;
+}
+
+
+struct block*insert_new_block(int blockid,struct block*prev,struct block*next){
+	struct block* newblock=new_block(blockid);
+	newblock->prev=prev;
+	newblock->next=next;
+	if(prev)prev->next=newblock;
+	if(next)next->prev=newblock;
+	return newblock;
+}
+
+
+struct block*get_block(int block_id){
+	//lookup a bigger blockid?go up the ll
+	if(block_id > block_cursor->id){
+		//seek while next exists
+		while(block_cursor->next){
+			//cases:  id< nextid, id==nextid, id>nextid:
+			//id < nextid? create a new block between curr and next 
+			//             and return it
+			if(block_id < block_cursor->next->id)
+				return insert_new_block(block_id,block_cursor,block_cursor->next);
+			//id==nextid? return nextblock
+			if(block_id == block_cursor->next->id)
+				return block_cursor->next;
+			//id>nextid? next
+			block_cursor=block_cursor->next;
+		}
+		//reached end without finding?add and return it
+		return insert_new_block(block_id,block_cursor,NULL);
+	}
+
+	//lookup a smaller blockid?go down the ll
+	if(block_id < block_cursor->id){
+		//seek while next exists
+		while(block_cursor->prev){
+			if(block_id > block_cursor->prev->id)
+				return insert_new_block(block_id, block_cursor->prev, block_cursor);
+			//id==nextid? return prevblock
+			if(block_id == block_cursor->prev->id)
+				return block_cursor->prev;
+			//id< previd? next
+			block_cursor=block_cursor->prev;
+		}
+		//reached end without finding?add and return it
+		return insert_new_block(block_id,block_cursor,NULL);
+	}
+	//id==cursor.id ? return it
+	return block_cursor;
+}
+
+
+void verify_clean(){
+	if(dirty_ptr){
+		set_cursor(get_block(block_of_addr(ptr)));
+	}
+}
 
 
 
@@ -62,11 +169,15 @@ char*ptr=NULL;           /*current memory pos. pointer*/
 *   and anything else that runs once at start of the interpreter
 */
 void initialize(int argc,char**argv){
-    code=malloc(code_size);
-    data=malloc(data_size);
+    
+    blocks=new_block(0);
+	set_cursor(blocks);
+	ptr=0;
+    cmd=code;
+    code =malloc(code_size);
     links=malloc(sizeof(char*)*code_size);
 
-    if(2==argc && '-'==argv[1][0] && 'v'==argv[1][1]){
+    if( 2==argc && '-'==argv[1][0] && 'v'==argv[1][1] ){
         verbose=1;
     }
 }
@@ -82,10 +193,8 @@ int main(int argc,char**argv){
 
     /*execcute every line of input as a separate bf program*/
     while(1){
-        /* data memory<-0 */
-        zeros(data,data_size);
         /*pointers point to start of arrays*/
-        ptr=data;
+        ptr=0;
         cmd=code;
 
         /*read bf program from input*/
@@ -100,7 +209,7 @@ int main(int argc,char**argv){
         printf("\nexecution complete\n");
 
         /*wipe code to prepare for next line*/
-        zeros(code,code_size);
+        zerosc(code,code_size);
     }
 
     return 0;
@@ -125,6 +234,7 @@ void read_program(){
                 code_size = cmd-code;
                 cmd=code;
                 return;
+            
             default:
                 /*normal instruction? proceed to next mem.address*/
                 cmd++;
@@ -147,7 +257,8 @@ void read_program(){
 int execute(){
     execution_state=EXEC_CONTINUE;
 
-    while(EXEC_CONTINUE==execution_state){
+    while(EXEC_CONTINUE == execution_state){
+      //  printstate();
         exec_cmd();
     }
     return execution_state;
@@ -161,19 +272,20 @@ int execute(){
 *   update and return execution state
 */
 int exec_cmd(){
-
+   // putchar(*cmd);
+  //  putchar('\n');
     switch(*cmd){
         /*cmd points to instruction? 
           exec accordingly*/
-        case '>':exec_mem_right();break;
-        case '<': exec_mem_left();break;
-        case '+': exec_inc_mem(); break;
-        case '-': exec_dec_mem(); break;
-        case '.': exec_putchar(); break;
-        case ',': exec_getchar(); break;
-        case ':': exec_putnum();  break;
-        case '[': exec_from();    break;
-        case ']': exec_to();      break;
+        case '>': exec_mem_right(); break;
+        case '<': exec_mem_left();  break;
+        case '+': exec_inc_mem();   break;
+        case '-': exec_dec_mem();   break;
+        case '.': exec_putchar();   break;
+        case ',': exec_getchar();   break;
+        case ':': exec_putnum();    break;
+        case '[': exec_from();      break;
+        case ']': exec_to();        break;
         case EOF:
         /*cmd points to end of program?
           complete. change exec state*/
@@ -193,53 +305,7 @@ int exec_cmd(){
     return execution_state;
 }
 
-/*
-*   go forward to matching closing bracket,
-*   returns null if reaches end of code array without finding it
-*   returns address of closing bracket otherwise
-*/
-char* seek_closing_bracket_iter(){
-    char*tmp=cmd;
-    int scope=1;
 
-    while(scope!=0){
-        tmp++;
-        switch(*tmp){
-            case ']':scope--;break;
-            case '[':scope++;break;
-            case EOF:
-            fprintf(stderr,"missing closing bracket (%ld);\n",(long)(cmd-code));
-            return NULL;
-        }
-    }
-    return tmp;
-}
-
-
-
-/*
-*   go backwards to matching opening bracket,
-*   returns null if passes start of code array without finding it
-*   returns address of closing bracket otherwise
-*/
-char* seek_opening_bracket_iter(){
-    char*tmp=cmd;
-    int scope=1;
-
-    while(scope!=0){
-        tmp--;
-        if(tmp<=code){
-            fprintf(stderr,"missing opening bracket (%ld);\n",(long)(cmd-code));
-            execution_state=EXEC_STOPPED_ERR;
-            return NULL;
-        }
-        switch(*tmp){
-            case ']':scope++;break;
-            case '[':scope--;break;
-        }
-    }
-    return tmp;
-}
 
 
 char* seek_opening_bracket(char*cmd_ptr){
@@ -267,7 +333,7 @@ char* seek_closing_bracket(char*cmd_ptr){
 }
 
 /*reset an array to zeros */
-void zeros(char*arr,size_t len){
+void zerosc(char*arr,size_t len){
     while(len-->0){
         arr[len]=0;
     }
@@ -277,67 +343,62 @@ void zeros(char*arr,size_t len){
 
 /*move data pointer right */
 int exec_mem_right(){
-    ++ ptr; 
-    if(ptr-data >= data_size){
-        data_size+=block_size;
-        data=realloc(data,data_size);
-    }
+    ++ ptr;
+    dirty_ptr=1;
 
-    if(verbose)
-        printf("(%ld)< mem[%ld]:%c\n",(long)(cmd-code),(long)(ptr-data),*ptr);
     return EXEC_CONTINUE;           
 }
 
 /*move data pointer left*/
 int exec_mem_left(){
     -- ptr;
-    
-    if(verbose)
-        printf("(%ld)< mem[%ld]:%c\n",(long)(cmd-code),(long)(ptr-data),*ptr);
-    
-    if(ptr<data){
-        printf("error:data underrun (%ld)< mem[%ld]\n",(long)(cmd-code),(long)(ptr-data));
-        execution_state=EXEC_STOPPED_ERR;
-        return EXEC_STOPPED_ERR;
-    }
+    dirty_ptr=1;
+
     return EXEC_CONTINUE;
 }
 
 /*increase value of selected mem.pos*/
 int exec_inc_mem(){
-    ++*ptr;
+	verify_clean();
+	block_mem[ ptr%block_size ]++;
     return EXEC_CONTINUE;
 }
 
 /*decrease value of selected mem.pos*/
 int exec_dec_mem(){
-    --*ptr;
+    verify_clean();
+	block_mem[ ptr%block_size ]--;
     return EXEC_CONTINUE;
 }
 
 
 /*curr mem <- getchar*/
 int exec_getchar(){
-    *ptr=getchar();
+    verify_clean();
+    block_mem[ ptr%block_size ]=getchar();
     return EXEC_CONTINUE;
 }
 
 /*putchar <- curr mem value*/
 int exec_putchar(){
-    putchar(*ptr);
+    verify_clean();
+    putchar(block_mem[ ptr%block_size ]);
     return EXEC_CONTINUE;
 }
 
 /*print mem value as a number*/
 int exec_putnum(){
-    printf("%d ",*ptr);
+    verify_clean();
+    printf("%d ",block_mem[ ptr%block_size ]);
     return EXEC_CONTINUE;
 }
 
 
 /*skip forward to closing bracket if ptr value=zero */
 int exec_from(){
-    if(0==*ptr){
+    verify_clean();
+    
+    if( 0==block_mem[ ptr%block_size ]){
         cmd=seek_closing_bracket(cmd);
         if(NULL==cmd)
             return EXEC_STOPPED_ERR;
@@ -348,18 +409,27 @@ int exec_from(){
 
 /*return back to opening bracket if ptr value=nonzero */
 int exec_to(){
-    if(0!=*ptr){
+    verify_clean();
+    
+    if( 0 != block_mem[ ptr%block_size ] ){
         cmd=seek_opening_bracket(cmd);
         if(NULL==cmd)
             return EXEC_STOPPED_ERR;
     }
+    
     return EXEC_CONTINUE;
 }
 
 
-
+/*  
+ *  for each '[' in code,place its adress in links[i]
+ *  for each ']' in code, place its address in links[i+1],
+ *   where links[i] is the matching opening bracket
+ *
+ * */
 int link_brackets(char*code,char**links){
-    char* stack[2000];
+    int buffsize=2048;
+    char** stack=malloc(sizeof(char*)*buffsize);
     int sp=0;
     int stack_size=0;
     int i=0;
@@ -368,6 +438,10 @@ int link_brackets(char*code,char**links){
     while(EOF != code[i]){
         switch(code[i]){
             case '[':
+		if(sp>=buffsize){
+			buffsize+=2048;
+			stack=realloc(stack,buffsize);
+		}
                 stack[sp++]=code+i;
                 break;
 
@@ -379,5 +453,6 @@ int link_brackets(char*code,char**links){
         }
         i++;
     }
+    if(stack)free(stack);
     return lp;
 }
